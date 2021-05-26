ce2e33b4a542bb91618f9a703355b514bb788a4a
RustamNossov <rustamn48@gmail.com>
2021-05-26 17:13:23 +0600

    Timer added

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import modals from './modules/modals';
 import {tabs} from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () =>{
     "use strict";
@@ -16,5 +17,8 @@ window.addEventListener('DOMContentLoaded', () =>{
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     forms(modalState);
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+    
+    let deadline = '2021-07-01';
+    timer('.container1', deadline);
 });
 
